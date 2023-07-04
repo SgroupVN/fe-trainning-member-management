cd4d0d9b0d47ec8055a799c3a48c295d8f0f2ada
MinhDong <minhdong1831999@gmail.com>
2023-07-04 16:59:02 +0700

    2nd commit

--- index.js
+++ index.js
@@ -4,24 +4,33 @@ createApp({
     data() {
         return {
             reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+            avatar: 'https://i.pravatar.cc/300',
             user: {
                 name: {
                     label: 'name',
                     value: 'Test name',
+                    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>'
                 },
                 email: {
                     label: 'email',
-                    value: 'email@gmail.com'
+                    value: 'email@gmail.com',
+                    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>'
                 },
                 city: {
                     label: 'city',
                     value: 'Da Nang',
-                    type: 'select'
+                    type: 'select',
+                    icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>'
                 },
                 major: {
                     label: 'Major',
                     value: ['IT'],
                     type: 'checkbox'
+                },
+                gender: {
+                    label: 'Gender',
+                    value: 'Male',
+                    type: 'radio'
                 },
             },
             cities: [
@@ -33,6 +42,11 @@ createApp({
                 'IT',
                 'DS',
                 'MO'
+            ],
+            gender: [
+                'Male',
+                'Female',
+                'Apache Helikopter'
             ]
         }
     },
