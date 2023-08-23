b9c8597ab8569fa10d31b31b677c6360339d169c
minhcu <minhdong1831999@gmail.com>
2023-08-23 16:09:52 +0700

    route example

--- src/router/index.js
+++ src/router/index.js
@@ -15,12 +15,34 @@ const routes = [
         name: 'user-list',
         component: () => import('@/views/dashboard/users/UserList.vue'),
       },
+      {
+        path: 'users/:id',
+        name: 'user-detail',
+        component: () => import('@/views/dashboard/users/UserDetail.vue'),
+      },
+      {
+        path: 'polls',
+        name: 'polls',
+      },
+      {
+        path: 'polls/:id',
+        name: 'poll detail',
+      },
+      {
+        path: 'settings',
+        name: 'settings',
+      },
     ],
   },
   {
     path: '/login',
     name: 'login',
     component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not found',
+    component: () => import('@/views/NotFound.vue'),
   },
 ]
 
@@ -30,6 +52,13 @@ const router = createRouter({
     return { top: 0 }
   },
   routes,
+})
+
+router.beforeEach((to) => {
+  const token = localStorage.getItem('accessToken')
+
+  if (to.name !== 'login' && to.name !== 'not found' && !token) return '/login'
+  if (token && to.name === 'login') return '/'
 })
 
 export default router
